b25b6eabcb0dab1e7891e96cca2cf0fe8c4a8a89
gracetveit <gracetveit@users.noreply.github.com>
2022-01-05 19:17:29 +0000

    Prettified Code!

--- cypress/integration/backend.spec.ts
+++ cypress/integration/backend.spec.ts
@@ -1,10 +1,10 @@
-describe("API testing", () => {
+describe('API testing', () => {
   it('returns hello world', () => {
     cy.request('http://localhost:3000/api').as('helloWorld');
     cy.get('@helloWorld').should((response: any) => {
       expect(response.status).to.eq(200);
-      expect(response.body).to.eq('Hello World')
-    })
-  })
-})
-export{}
+      expect(response.body).to.eq('Hello World');
+    });
+  });
+});
+export {};
--- cypress/integration/frontend.spec.ts
+++ cypress/integration/frontend.spec.ts
@@ -1,8 +1,8 @@
 describe('Frontend', () => {
-  it("should navigate to the front page", () => {
-    cy.visit('http://localhost:3000')
+  it('should navigate to the front page', () => {
+    cy.visit('http://localhost:3000');
 
-    cy.get('h1').contains('Hello World')
-  })
-})
-export{}
+    cy.get('h1').contains('Hello World');
+  });
+});
+export {};
