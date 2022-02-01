9316266781d17672fc76291519780f4ff5fda4c5
Grace Tveit <grace.tveit@gmail.com>
2022-02-01 10:25:03 -0800

    refactor: grabbing POST data directly from body

--- pages/api/authors/index.ts
+++ pages/api/authors/index.ts
@@ -29,12 +29,16 @@ async function getAll(req: NextApiRequest, res: NextApiResponse) {
  * POST Request
  */
 async function post(req: NextApiRequest, res: NextApiResponse) {
-  const author = req.body.author;
+  const author = req.body;
+  if (!author) {
+    throw new Error('No author found');
+  }
   try {
     const createdAuthor = await prisma.author.create({ data: author });
     res.status(201).send(createdAuthor);
   } catch (error) {
     res.status(500).send({ error: error.message });
+    console.log(error.message);
   }
 }
 
