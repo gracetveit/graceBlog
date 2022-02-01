ec4523f70898962cfc815c9f737ae15b1762b33f
Grace Tveit <grace.tveit@gmail.com>
2022-01-31 16:42:46 -0800

    feat: set up first test in Jest

--- tests/api/author.test.ts
+++ tests/api/author.test.ts
@@ -1,8 +1,18 @@
 import request from 'supertest';
+import prisma from '../../prisma/client';
 
 describe('Author API', () => {
-  it('Returns all authors', () => {
-    request('http://localhost:3000/api').get('/').expect(200);
+  beforeEach(async () => {
+    await prisma.author.deleteMany();
+  });
+
+  it('Returns all authors', async () => {
+    await prisma.author.create({ data: { name: 'test' } });
+    const res = await request('http://localhost:3000/api')
+      .get('/authors')
+      .expect(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body[0].name).toBe('test');
   });
 });
 
