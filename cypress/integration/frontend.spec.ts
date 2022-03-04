8be1821e65d0ca673cd26dc40ac330c8e8a48393
Grace Tveit <grace.tveit@gmail.com>
2022-03-04 11:32:16 -0800

    resetting repo

--- cypress/integration/frontend.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('Frontend', () => {
-  it('should navigate to the front page', () => {
-    cy.visit('http://localhost:3000');
-
-    cy.get('h1').contains('Hello World');
-  });
-});
-export {};
