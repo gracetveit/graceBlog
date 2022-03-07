6ffd9cde70a69e3d93e68e2bbca4873195faade7
Grace Tveit <grace.tveit@gmail.com>
2022-03-07 12:52:43 -0800

    adding api routes

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
-
 import path from 'path';
+
+import apiRouter from './api';
 
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
+router.use('/api', apiRouter);
 
 export default router;
