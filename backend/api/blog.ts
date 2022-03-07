e963270bac818437baa87e274ca11e731c3dac7f
Grace Tveit <grace.tveit@gmail.com>
2022-03-07 15:49:53 -0800

    feat: Adding backend route to find specific posts

--- backend/api/blog.ts
+++ backend/api/blog.ts
@@ -16,6 +16,24 @@ const getAll = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+/**
+ * Get Single Blog from a slug
+ *
+ * **Request Params**
+ * - slug: slugified string
+ */
+const getFromSlug = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const blog = await prisma.post.findUnique({
+      where: { slug: req.params.slug },
+    });
+    res.send(blog);
+  } catch (error) {
+    next(error);
+  }
+};
+
 router.get('/', getAll);
+router.get('/:slug', getFromSlug);
 
 export default router;
