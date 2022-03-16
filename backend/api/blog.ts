2e665c8687c9847b48bf73001b8307812c044816
Grace Tveit <grace.tveit@gmail.com>
2022-03-16 11:59:04 -0700

    refactor: moving the rest of the blog CRUD to the blog api route

--- backend/api/blog.ts
+++ backend/api/blog.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import { Post } from '@prisma/client';
 
 import prisma from '../prisma/client';
+import { validate } from './auth';
 
 /**
  * GET ALL blog posts
@@ -60,9 +61,50 @@ const getMostRecent = async (
     next(error);
   }
 };
+// POST
+const createBlog = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const blog: Partial<Post> = req.body;
+    await prisma.post.create({
+      data: {
+        title: blog.title!,
+        content: blog.content!,
+        slug: encodeURI(blog.title!),
+      },
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    next(error);
+  }
+};
+// PUT
+const updateBlog = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = parseInt(req.params.id);
+    const blog: Post = req.body;
+    await prisma.post.update({ where: { id }, data: blog });
+    res.sendStatus(201);
+  } catch (error) {
+    next(error);
+  }
+};
+// DELETE
+const deleteBlog = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = parseInt(req.params.id);
+    await prisma.post.delete({ where: { id } });
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
 
 router.get('/', getAll);
 router.get('/most-recent', getMostRecent);
 router.get('/:slug', getFromSlug);
+
+router.post('/blog', validate, createBlog);
+router.put('/blog/:id', validate, createBlog);
+router.delete('/blog/:id', validate, deleteBlog);
 
 export default router;
