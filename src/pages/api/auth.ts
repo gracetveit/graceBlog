11342c485e2df684be9b6138b85f1119a8c4bf6a
Grace Tveit <grace.tveit@gmail.com>
2022-03-31 10:54:14 -0700

    feat: adding backend login route

---
+++ src/pages/api/auth.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from ".prisma/client";
+import * as argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const login = async (req: NextApiRequest, res: NextApiResponse) => {
+  const db = new PrismaClient();
+  const { username, password } = req.body;
+  try {
+    const user = await db.user.findUnique({
+      where: { username },
+      rejectOnNotFound: true,
+    });
+
+    if (!(await argon2.verify(user.pwHash, password))) {
+      throw new Error("Incorrect Password");
+    }
+
+    const token = jwt.sign(user, process.env.SECRET);
+    await db.user.update({
+      where: { username },
+      data: { token },
+    });
+    res.json(token);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case "POST":
+      await login(req, res);
+    default:
+      res.status(405);
+  }
+};
