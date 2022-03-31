e9fced840f1a4b5e6fbe3fe6b6bcf285c9631ccb
Grace Tveit <grace.tveit@gmail.com>
2022-03-31 10:33:49 -0700

    feat: adding intial user to seed

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import * as argon2 from "argon2";
+import "dotenv/config";
 
 const db = new PrismaClient();
 
@@ -19,8 +21,26 @@ const post = async () => {
   }
 };
 
+const user = async () => {
+  try {
+    await db.user.deleteMany();
+
+    await db.user.create({
+      data: {
+        username: process.env.CLIENT_USERNAME,
+        pwHash: await argon2.hash(process.env.CLIENT_PASSWORD),
+        email: process.env.EMAIL,
+      },
+    });
+    console.log("User created!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const seed = async () => {
   await post();
+  await user();
 };
 
 seed()
