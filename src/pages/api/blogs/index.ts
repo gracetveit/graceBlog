fc47d9faf0b7702d8459a6d336a036dcdf541345
Grace Tveit <grace.tveit@gmail.com>
2022-04-01 10:52:37 -0700

    feat: adding createBlog backend route

--- src/pages/api/blogs/index.ts
+++ src/pages/api/blogs/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from ".prisma/client";
+import { Blog, PrismaClient } from ".prisma/client";
+import { verify } from "../auth";
 
 const db = new PrismaClient();
 
@@ -8,11 +9,25 @@ const getAll = async (res: NextApiResponse) => {
   res.status(200).json(blogs);
 };
 
+const create = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const blog: Blog = req.body;
+    const createdBlog = await db.blog.create({ data: blog });
+    res.json(createdBlog);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case "GET":
       await getAll(res);
+      break;
+    case "POST":
+      verify(req, res, create);
+      break;
     default:
-      res.status(405);
+      res.status(405).end();
   }
 };
