d8473424bd3a05e6ac00facf63bff46e8e4e15ee
Grace Tveit <grace.tveit@gmail.com>
2022-04-04 11:40:52 -0700

    adding auth check to new-blog page

--- src/pages/admin/new-blog.tsx
+++ src/pages/admin/new-blog.tsx
@@ -1,6 +1,11 @@
+import AuthCheck from "../../components/AuthCheck";
 import BlogForm from "../../components/BlogForm";
 import { createBlog } from "../../store/singleBlog";
 
 export default () => {
-  return <BlogForm method="POST" thunk={createBlog} />;
+  return (
+    <AuthCheck>
+      <BlogForm method="POST" thunk={createBlog} />
+    </AuthCheck>
+  );
 };
