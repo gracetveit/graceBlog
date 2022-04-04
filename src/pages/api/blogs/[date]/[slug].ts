3e57c6f1858f7e83f70448d9dc2ca6875db14968
Grace Tveit <grace.tveit@gmail.com>
2022-04-04 12:14:59 -0700

    feat: adding delete route

--- src/pages/api/blogs/[date]/[slug].ts
+++ src/pages/api/blogs/[date]/[slug].ts
@@ -1,15 +1,34 @@
 import { PrismaClient } from ".prisma/client";
+import { Verified } from "@mui/icons-material";
 import { NextApiRequest, NextApiResponse } from "next";
+import { verify } from "../../auth";
 
 const db = new PrismaClient();
 
-const getSingle = async (req: NextApiRequest, res: NextApiResponse) => {
+type BlogParams = {
+  slug: string;
+  gte: Date;
+  lt: Date;
+};
+
+const getParams = (req: NextApiRequest, res: NextApiResponse): BlogParams => {
   try {
     let { date, slug } = req.query;
     [date, slug] = [date as string, slug as string];
     const gte = new Date(date);
     const lt = new Date(date);
     lt.setDate(gte.getDate() + 1);
+    return { slug, gte, lt };
+  } catch (error) {
+    res.status(500).json({ status: 404, error });
+  }
+};
+
+const getSingle = async (
+  { gte, lt, slug }: BlogParams,
+  res: NextApiResponse
+) => {
+  try {
     const blog = await db.blog.findFirst({
       where: {
         createdAt: {
@@ -27,11 +46,40 @@ const getSingle = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
+const deleteBlog = async (
+  { gte, lt, slug }: BlogParams,
+  res: NextApiResponse
+) => {
+  try {
+    await db.blog.deleteMany({
+      where: {
+        createdAt: {
+          gte,
+          lt,
+        },
+        slug,
+      },
+    });
+    res.status(204).end();
+  } catch (error) {
+    console.error(error);
+    res.status(error.status).json({ status: error.status, error: error });
+  }
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const blogParams = getParams(req, res);
   switch (req.method) {
     case "GET":
-      await getSingle(req, res);
+      await getSingle(blogParams, res);
+      break;
+    case "DELETE":
+      const verified = await verify(req, res);
+      if (verified) {
+        await deleteBlog(blogParams, res);
+      }
+      break;
     default:
-      res.status(405);
+      res.status(405).end();
   }
 };
