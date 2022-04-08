327cab75eb5c205b3ab36653c1985fa2fda8cf18
Grace Tveit <grace.tveit@gmail.com>
2022-04-08 13:52:39 -0700

    style: making SingleBlog a bit more dynamic

--- src/components/BlogRead.tsx
+++ src/components/BlogRead.tsx
@@ -5,12 +5,21 @@ import ReactMarkdown from "react-markdown";
 export default ({ blog }) => {
   const date = (blog.createdAt as unknown as string).split("T")[0];
   return (
-    <Box margin="20px" maxWidth="90vw" width="1200px">
+    <Box
+      margin="20px"
+      maxWidth="90vw"
+      width="1200px"
+      display="flex"
+      flexDirection="column"
+      gap="10px"
+    >
       <Paper sx={{ padding: "10px" }}>
         <Box display="flex" justifyContent="space-between">
           <Typography variant="h1">{blog.title}</Typography>
           <Typography variant="subtitle1">{date}</Typography>
         </Box>
+      </Paper>
+      <Paper sx={{ padding: "10px" }}>
         <Box>
           <Typography align="left">
             <ReactMarkdown>{blog.content}</ReactMarkdown>
