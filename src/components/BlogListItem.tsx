6c7a7c7d020732cbe88623c1b496bffb8aeacaca
Grace Tveit <grace.tveit@gmail.com>
2022-04-08 12:16:12 -0700

    feat: Prettifying BlogListItem

--- src/components/BlogListItem.tsx
+++ src/components/BlogListItem.tsx
@@ -1,6 +1,8 @@
 import { Blog } from ".prisma/client";
-import { Button } from "@mui/material";
+import { Button, ButtonBase, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 type BlogListItemProps = {
   blog: Blog;
@@ -8,14 +10,45 @@ type BlogListItemProps = {
 };
 
 export default ({ blog, isAdmin }: BlogListItemProps) => {
+  const router = useRouter();
+
   const date = (blog.createdAt as unknown as string).split("T")[0];
   const link = !isAdmin
     ? `/blogs/${date}/${blog.slug}`
     : `/admin/${date}/${blog.slug}`;
   return (
-    <div>
-      <Link href={link}>{blog.title}</Link>
-      {!isAdmin ? <></> : <Button>Delete</Button>}
-    </div>
+    <ButtonBase
+      sx={{ flexGrow: 1, display: "flex" }}
+      onClick={() => {
+        router.push(link);
+      }}
+    >
+      <Paper sx={{ flexGrow: 1, padding: "10px", gap: "20px" }}>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="h1">{blog.title}</Typography>
+          {!isAdmin ? (
+            <></>
+          ) : (
+            <Button
+              variant="contained"
+              color="error"
+              sx={{ height: "fit-content" }}
+              onClick={(e) => {
+                e.stopPropagation();
+                console.log("test");
+              }}
+            >
+              Delete
+            </Button>
+          )}
+        </Box>
+        <Typography variant="subtitle1" align="left">
+          {date}
+        </Typography>
+        <Box>
+          <Typography align="left">{blog.content.slice(0, 150)}...</Typography>
+        </Box>
+      </Paper>
+    </ButtonBase>
   );
 };
