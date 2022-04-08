f20ff152b05adac217378e51a9fac7b7261654ff
Grace Tveit <grace.tveit@gmail.com>
2022-04-08 13:19:53 -0700

    feat: adding store delete action

--- src/store/allBlogs.ts
+++ src/store/allBlogs.ts
@@ -3,11 +3,17 @@ import axios from "axios";
 
 // Constants
 const SET_BLOGS = "SET_BLOGS";
+const REMOVE_BLOGS = "REMOVE_BLOGS";
 
 // Actions
 const setBlogs = (blogs) => ({
   type: SET_BLOGS,
   blogs,
+});
+
+const removeBlogs = (slug) => ({
+  type: REMOVE_BLOGS,
+  slug,
 });
 
 import { Dispatch } from "react";
@@ -29,11 +35,28 @@ export const fetchBlogs = () => async (dispatch) => {
     console.error(error);
   }
 };
+
+export const deleteBlogs = (blog: Blog) => async (dispatch) => {
+  const date = (blog.createdAt as unknown as string).split("T")[0];
+  try {
+    await axios.delete(`/api/blogs/${date}/${blog.slug}`);
+    dispatch(removeBlogs(blog.slug));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 // Reducer
 export default (state: clientBlog[] = [], action): clientBlog[] => {
   switch (action.type) {
     case SET_BLOGS:
       return action.blogs;
+    case REMOVE_BLOGS:
+      return state.map((blog) => {
+        if (blog.slug !== action.slug) {
+          return blog;
+        }
+      });
     default:
       return state;
   }
