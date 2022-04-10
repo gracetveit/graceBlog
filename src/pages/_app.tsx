247dd4450912d97739299697a6a0c2cfad0ebea9
Grace Tveit <grace.tveit@gmail.com>
2022-04-09 21:41:35 -0700

    feat: adding favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Box } from "@mui/system";
 import "../../styles.css";
+import Head from 'next/head';
 
 export default ({ Component, pageProps }) => {
   const theme = createTheme({
@@ -35,6 +36,31 @@ export default ({ Component, pageProps }) => {
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Head>
+          <title>graceBlog</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <Layout>
           <Box display="flex" flexDirection="column" alignItems="center">
             <Component {...pageProps} />
